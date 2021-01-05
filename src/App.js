5ab0862930a9cb50a61147106ddec1fdeb74fa4c
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2021-01-05 12:39:41 +0100

    Subscribe to topic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 /* global ROSLIB */
+import 'eventemitter2/lib/eventemitter2';
 import 'roslib/build/roslib';
-// import 'eventemitter2/lib/eventemitter2';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ROS_WEBBRIDGE_SERVER } from './constants';
 
 import './App.css';
 
 function App() {
 
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     var ros = new ROSLIB.Ros();
 
-    ros.on('error', error => console.log('Error! 💥'));
-    ros.on('connection', error => console.log('Connected! ✅'));
-    ros.on('close', error => console.log('Connection closed! ❌'));
+    ros.on('error', () => console.log('Error! 💥'));
+    ros.on('connection', () => console.log('Connected! ✅'));
+    ros.on('close', () => console.log('Connection closed! ❌'));
 
     ros.connect(ROS_WEBBRIDGE_SERVER);
-  })
+
+    const position = new ROSLIB.Topic({
+      ros : ros,
+      name : '/joint_position',
+      messageType : 'pr_msgs/PRArrayH'
+    });
+
+    position.subscribe(message => setData(message.data));
+  }, []);
 
   return (
     <>
       <main className="main">
         <h1>PR Web</h1>
         <button>Click me!</button>
+        {data.map((item, index) => <div key={index}>{item}</div>)}
       </main>
     </>
   );
